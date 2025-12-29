f3d62e98dd273fcfd81ea78bf6259f46ca419a39
EastRain816 <dder2dder2@gmail.com>
2025-12-29 20:13:15 +0900

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
-        System.out.println("I,m Dongwoo");
+        System.out.printf("Hello and welcome!");
     }
 }
